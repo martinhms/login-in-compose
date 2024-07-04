366eba657043ea6b3239d459e4febba163ed5020
Martin Horacio Mendez <martinhmendez@MacBook-Pro-de-Martin.local>
2024-07-04 15:51:36 -0300

    [LOGIN] - Added header and body

---
+++ app/src/main/java/com/example/login_compose/Login.kt
@@ -0,0 +1,109 @@
+package com.example.login_compose
+
+import android.app.Activity
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+
+@Composable
+fun Login() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(20.dp)
+    ) {
+        HeaderLogin(Modifier.align(Alignment.TopEnd))
+        Body(Modifier.align(Alignment.Center))
+    }
+
+}
+
+@Composable
+fun Body(modifier: Modifier) {
+    var email by rememberSaveable {
+        mutableStateOf("")
+    }
+    var password by rememberSaveable {
+        mutableStateOf("")
+    }
+    Column(modifier = modifier) {
+        ImageLogo()
+        Spacer(modifier = Modifier.size(16.dp))
+        Email(email = email) { email = it }
+        Spacer(modifier = Modifier.size(4.dp))
+        Password(password = password) { password = it }
+        Spacer(modifier = Modifier.size(8.dp))
+        ForgotPassword(Modifier.align(Alignment.End))
+    }
+}
+
+@Composable
+fun ImageLogo() {
+    Image(painter = painterResource(id = R.drawable.insta), contentDescription = "logo")
+}
+
+@Composable
+fun Email(email: String, onTextChange: (String) -> Unit) {
+
+    TextField(value = email, onValueChange = { onTextChange(it) })
+}
+
+@Composable
+fun Password(password: String, onTextChange: (String) -> Unit) {
+
+    TextField(value = password, onValueChange = { onTextChange(it) })
+}
+
+@Composable
+fun ForgotPassword(modifier: Modifier) {
+    Text(
+        text = "Forgot password?",
+        fontSize = 12.sp,
+        fontWeight = FontWeight.Bold,
+        color = Color.Blue,
+        modifier = modifier
+    )
+}
+
+
+@Composable
+fun HeaderLogin(modifier: Modifier) {
+    val activity = LocalContext.current as Activity
+    Icon(
+        imageVector = Icons.Default.Close,
+        contentDescription = "close app",
+        modifier = modifier.clickable { activity.finish() })
+
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun HeaderPreview() {
+
+}
